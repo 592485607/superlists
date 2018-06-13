0edb3dfe49a7c4b256915dbfb75657bb5a022536
felix <592485607@qq.com>
2018-06-13 23:03:19 +0800

    first commit

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
         #她注意到网页的标题和头部都包含了"To-Do"这个词
         self.assertIn('To-Do',self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
 
@@ -30,11 +31,12 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        #她在一个文本框中输入了"buy peacock feathers"
-        #伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        #她在一个文本框中输入了"buy peacock feathers"（购买孔雀羽毛）
+        #伊迪丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        #她按回车键后,页面更新了,待办事项表格中显示了"1:Buy peacock feathers"
+        #她按回车键后,页面更新了。
+        # 待办事项表格中显示了"1:Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
 
         import time
@@ -46,11 +48,23 @@ class NewVisitorTest(unittest.TestCase):
             any(row.text == '1:Buy peacock feathers' for row in rows)
         )
 
-        #页面中又显示了一个文本框,可以输入其他的事项
-        #她输入了:Use peacock feathers to make a fly"
+        #页面中又显示了一个文本框,可以输入其他的待办事项
+        #她输入了:Use peacock feathers to make a fly"（使用孔雀羽毛做假蝇）
         #伊迪丝做事很有条理
         self.fail('Finish the test!')
+
         # 页面再次更新,她的清单中显示了这两个待办事项
+
+        #伊迪丝想知道这个网站是否会记住她的清单
+
+        #她看到网站为她生成了一个唯一的URL
+        #而且页面中有一些文字解说这个功能
+
+        #她访问那个URL，发现她的待办事项列表还在
+
+        #她很满意，去睡觉了
+
+        self.browser.quit()
 
 if __name__ == '__main__':
     #warnings='ignore' 作用禁止抛出resourceWarning异常
