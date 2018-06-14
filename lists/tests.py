026c5fd0a85c4761b003d90209e4621ea4bbbf70
felix <592485607@qq.com>
2018-06-14 23:29:52 +0800

    all

--- lists/tests.py
+++ lists/tests.py
@@ -38,3 +38,17 @@ class HomePageTest(TestCase):
 
         #使用 .decode() 把 response.content 中的字节转换成 Python 中的 Unicode 字符串，这样就可以对比字符串，而不用像之前那样对比字节
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(),expected_html)
--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,18 @@ def home_page(request):
     # Django 中的 render 函数。这个函数的第一个参数是请求对象的，
     # 第二个参数是渲染的模板名。 Django 会自动在所有的应用目录中搜索名为 templates 的文件夹，
     # 然后根据模板中的内容构建一个 HttpResponse 对象
-    return render(request,'home.html')
+
+    # def home_page(request):
+    #     if request.method == 'POST':
+    #         return HttpResponse(request.POST['item_text'])
+    #         return render(request, 'home.html')
+
+    # def home_page(request):
+    #     return render(request, 'home.html', {
+    #         'new_item_text': request.POST['item_text'],
+    #     })
+
+    def home_page(request):
+        return render(request, 'home.html', {
+            'new_item_text': request.POST.get('item_text', ''),
+        })
